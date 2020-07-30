5e0825ba90ebfcc12d1daffa58c216c0e42e485a
Filipe Freire <filipefreire@protonmail.com>
2020-07-30 16:18:23 +0100

    Complete iteration 1

---
+++ views/models/place.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      enum: ['coffee_shop', 'bookstore']
+    }
+  },
+  {
+    timestamps: true
+  }
+);
